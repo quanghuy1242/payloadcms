b37f6429814a58c3a8dee203f413a1a032692043
Huy Quang Nguyen <quanghuy1242@gmail.com>
2025-10-24 03:50:41 +0700

    Add more permission to admin

--- src/utils/access.ts
+++ src/utils/access.ts
@@ -120,6 +120,10 @@ export const adminOrSelfFieldAccess: FieldAccess = (args) => {
 
 export const ownerAccess = (field: string): Access => {
   return ({ req }) => {
+    if (isAdminUser(req.user)) {
+      return true
+    }
+
     const userId = getUserId(req.user)
 
     if (userId == null) {
