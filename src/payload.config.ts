1c3c28ddad1635e80f9a8d29a2ab662f5c9999f1
Huy Quang Nguyen <quanghuy1242@gmail.com>
2025-10-23 23:05:20 +0700

    Add schema to graphql server

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -97,6 +97,7 @@ export default buildConfig({
   plugins: [...storagePlugins, seo],
   graphQL: {
     disablePlaygroundInProduction: false,
+    schemaOutputFile: path.resolve(dirname, '../schema.graphql'),
     queries,
   },
 })
